10152172ddcd694b9ac453b78f04d016b23af85b
Bryan Cusick <bryancusick@gmail.com>
2024-06-20 08:10:15 -0700

    cleaned up files

--- ETo.py
+++ ETo.py
@@ -1,9 +1,11 @@
-
 import requests
 import json
-import os
 import yaml
+import os
 from datetime import datetime, timedelta
+
+# Path to the JSON file
+file_path = f'ETo_{data_year}.json'
 
 with open('config.yaml', 'r') as file:
         config_data = yaml.safe_load(file)
@@ -12,8 +14,79 @@ KEY       = config_data['OPENet_KEY']
 latitude    = config_data['latitude']
 longitude   = config_data['longitude']
 
+def fetch(data_year): #returns true if there is new data
+    
+    complete = False
+    current_year = datetime.now().year
+    yesterday = datetime.now() - timedelta(days=1)
+
+    start_date = datetime(data_year, 1, 1)
+    end_date = datetime(data_year, 12, 31)
+
+    if data_year == current_year:
+        end_date = yesterday
+
+    # Check if the file exists
+    if os.path.exists(file_path):
+        # Load existing data
+        with open(file_path, 'r') as f:
+            existing_data = json.load(f)
+        
+        # Determine the last fetched date from existing data
+        if existing_data:
+            last_fetched_date_str = existing_data[-1]['time']
+            last_fetched_date = datetime.strptime(last_fetched_date_str, "%Y-%m-%d")
+
+            if last_fetched_date == end_date:
+                complete = True #dataset complete
+            else:
+                start_date = last_fetched_date + timedelta(days=1)
+    else:
+        existing_data = []
+
+    # Fetch new data - this section API Specific
+    if not complete:
+        
+        new_data = call_API(start_date, end_date)
+        #pull entire history again and overwrite.  
+        try:
+            with open(file_path, 'w') as f:
+                json.dump(new_data, f, indent=4)
+            #print(f"Data successfully saved to {file_path}")
+        except IOError as e:
+            print(f"Error writing to file: {e}")
+        
+        retval = 1
+
+    else:
+        retval = 0
+
+    return retval
+
 # Function to fetch data from API
-def call_API(args):
+def call_API(start_date, end_date):
+
+    start_date_str = start_date.strftime('%Y-%m-%d')
+    end_date_str = end_date.strftime('%Y-%m-%d')
+    date_range = [
+            start_date_str,
+            end_date_str]
+
+    # Endpoint arguments
+    args = {
+        "date_range": date_range,
+        "interval": "daily",
+        "geometry": [
+            longitude,
+            latitude
+        ],
+        "model": "Ensemble",
+        "variable": "ETo",
+        "reference_et": "CIMIS",
+        "units": "in",
+        "file_format": "JSON"
+    }
+
     # set your API key before making the request
     header = {"Authorization": KEY}
 
@@ -29,78 +102,6 @@ def call_API(args):
     except requests.exceptions.RequestException as e:
         print(f"Error querying the API: {e}")
         return None
-
-def fetch(data_year): #returns true if there is new data
-    
-    complete = False
-    current_year = datetime.now().year
-    yesterday = datetime.now() - timedelta(days=1)
-    yesterday_str = yesterday.strftime('%Y-%m-%d')
-    start_date_str = f"{data_year}-01-01"
-    end_date_str = f"{data_year}-12-31"
-
-    # setup date range to pass to API
-    date_range = [
-            start_date_str,
-            end_date_str]
-
-    if data_year == current_year:
-        date_range[1] = yesterday_str
-        
-    # Endpoint arguments
-    args = {
-        "date_range": date_range,
-        "interval": "daily",
-        "geometry": [
-            longitude,
-            latitude
-        ],
-        "model": "Ensemble",
-        "variable": "ETo",
-        "reference_et": "CIMIS",
-        "units": "in",
-        "file_format": "JSON"
-    }
-  
-    # Path to the JSON file
-    file_path = f'ETo_{data_year}.json'
-
-    # Check if the file exists
-    if os.path.exists(file_path):
-        # Load existing data
-        with open(file_path, 'r') as f:
-            existing_data = json.load(f)
-        
-        # Determine the last fetched date from existing data
-        if existing_data:
-            last_fetched_date_str = existing_data[-1]['time']
-            last_fetched_date = datetime.strptime(last_fetched_date_str, "%Y-%m-%d")
-
-            if last_fetched_date_str == f'{data_year}-12-31' or last_fetched_date == yesterday:
-                complete = True #dataset complete
-            else:
-                args["date_range"] = [start_date_str, yesterday_str]
-    else:
-        existing_data = []
-
-    # Fetch new data
-    if not complete:
-        print(args)
-        new_data = call_API(args)
-        
-        retval = 1
-        try:
-            with open(file_path, 'w') as f:
-                json.dump(new_data, f, indent=4)
-            #print(f"Data successfully saved to {file_path}")
-        except IOError as e:
-            print(f"Error writing to file: {e}")
-        
-    else:
-        retval = 0
-
-    return retval
-
 
 ###############################################################################
 
--- weather.py
+++ weather.py
@@ -1,8 +1,11 @@
 import requests
 import json
-import os
 import yaml
+import os
 from datetime import datetime, timedelta
+
+# Path to the JSON file
+file_path = f'weather_{data_year}.json'
 
 with open('config.yaml', 'r') as file:
         config_data = yaml.safe_load(file)
@@ -10,6 +13,60 @@ with open('config.yaml', 'r') as file:
 KEY       = config_data['OPENWeather_KEY'] 
 latitude    = config_data['latitude']
 longitude   = config_data['longitude']
+
+def fetch(data_year): #returns true if there is new data
+    
+    complete = False
+    current_year = datetime.now().year
+    yesterday = datetime.now() - timedelta(days=1)
+
+    start_date = datetime(data_year, 1, 1)
+    end_date = datetime(data_year, 12, 31)
+
+    if data_year == current_year:
+        end_date = yesterday
+
+    # Check if the file exists
+    if os.path.exists(file_path):
+        # Load existing data
+        with open(file_path, 'r') as f:
+            existing_data = json.load(f)
+        
+        # Determine the last fetched date from existing data
+        if existing_data:
+            last_fetched_date_str = existing_data[-1]['date']
+            last_fetched_date = datetime.strptime(last_fetched_date_str, "%Y-%m-%d")
+
+            if last_fetched_date == end_date:
+                complete = True #dataset complete
+            else:
+                start_date = last_fetched_date + timedelta(days=1)
+    else:
+        existing_data = []    
+           
+    # Fetch new data - this section API Specific
+    if not complete:
+        fetch_date = start_date
+
+        while fetch_date <= end_date:
+            print(f"Adding data for {fetch_date}")
+            new_data = call_API(fetch_date)  
+            #just pull new data and append      
+            existing_data.append(new_data)
+            fetch_date += timedelta(days=1)
+        try:
+            with open(file_path, 'w') as f:
+                json.dump(existing_data, f, indent=4)
+            
+        except IOError as e:
+            print(f"Error writing to file: {e}")
+
+        retval = 1
+      
+    else:
+      retval = 0
+
+    return retval
 
 # Function to fetch data from API
 def call_API(date):
@@ -27,66 +84,6 @@ def call_API(date):
     except requests.exceptions.RequestException as e:
         print(f"Error querying the API: {e}")
         return None
-
-def fetch(data_year): #returns true if there is new data
-    
-    complete = False
-    current_year = datetime.now().year
-    today_date = datetime.now()
-
-    # Define the date range for this year
-    start_date = datetime(data_year, 1, 1)
-    end_date = datetime(data_year, 12, 31)
-
-    # Determine the date range based on whether data_year is the current year
-    if data_year == current_year:
-        end_date = today_date - timedelta(days=1)
-
-    # Path to the JSON file
-    file_path = f'weather_{data_year}.json'
-
-    # Check if the file exists
-    if os.path.exists(file_path):
-        # Load existing data
-        with open(file_path, 'r') as f:
-            existing_data = json.load(f)
-        
-        # Determine the last fetched date from existing data
-        if existing_data:
-            last_fetched_date_str = existing_data[-1]['date']
-            last_fetched_date = datetime.strptime(last_fetched_date_str, "%Y-%m-%d")
-
-            if last_fetched_date_str == f'{data_year}-12-31' or last_fetched_date == today_date - timedelta(days=1):
-                complete = True #dataset complete
-            else:
-                start_date = last_fetched_date + timedelta(days=1)
-    else:
-        existing_data = []    
-           
-    # Fetch new data
-    if not complete:
-        fetch_date = start_date
-
-        while fetch_date <= end_date:
-            print(f"Adding data for {fetch_date}")
-            new_data = call_API(fetch_date)        
-            existing_data.append(new_data)
-            fetch_date += timedelta(days=1)
-        try:
-            with open(file_path, 'w') as f:
-                json.dump(existing_data, f, indent=4)
-            
-        except IOError as e:
-            print(f"Error writing to file: {e}")
-
-        retval = 1
-      
-    else:
-      retval = 0
-
-    return retval
-
-
 ###############################################################################
 
 
